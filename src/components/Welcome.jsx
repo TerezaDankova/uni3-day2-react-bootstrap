74ec74e3566a19e55e8d5ef9a1dd928716823212
Tereza Dankova <104557274+TerezaDankova@users.noreply.github.com>
2022-09-06 17:05:41 +0200

    h1 change

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,18 +1,18 @@
 
-import { Container, Jumbotron} from "react-bootstrap";
+import { Jumbotron, Container} from "react-bootstrap";
 
 
 
-const Welcome =({title, text, image})=> {
+const Welcome =({title, text})=> {
       return(
     
             <Jumbotron style={{backgroundColor: "#B55B7F"}}>
                   <Container>
-                        
-                        <h1 className='text-light'>{title}</h1>
+                        <h1 className='text-light' style={{fontSize: 90}} >{title}</h1>
                         <p className='text-light'>{text}</p>
                   </Container>
             </Jumbotron>
      
 )}
+
 export default Welcome
